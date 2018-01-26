1fa1fc6ce9c7f6d5cbcce83e094159f3425fe902
zhaiyongyi <zhaiyongyi@henhaoji.com>
2018-01-26 18:17:58 +0800

    带参数的拦截器验证 old: * struts2 使用拦截器实现login验证小demo

--- eclipse-workspace/Struts2_InterceptorDemo/src/com/zyy/interceptor/LoginInterceptor.java
+++ eclipse-workspace/Struts2_InterceptorDemo/src/com/zyy/interceptor/LoginInterceptor.java
@@ -7,12 +7,23 @@ import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
 public class LoginInterceptor extends AbstractInterceptor {
+	
+	private String value;
+	
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 
 	@Override
 	public String intercept(ActionInvocation ai) throws Exception {
 
 		//此处判断有没有登录，如果没有登录跳回登录，如果已经登录了，那就跳到成功页面
-		
+		System.out.println("value="+value);
 		ActionContext ac = ActionContext.getContext();
 		Map session = ac.getSession();
 		String username = (String)session.get("loginUser");
