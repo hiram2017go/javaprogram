95a6f48dbae87289ac688247b6de061459cc888a
zhaiyongyi <zhaiyongyi@henhaoji.com>
2018-03-05 09:10:45 +0800

    修改添加jar包为本地文件夹下包

--- eclipse-workspace/Spring_AOP/src/com/zyy/dao/impl/UserDaoImpl.java
+++ eclipse-workspace/Spring_AOP/src/com/zyy/dao/impl/UserDaoImpl.java
@@ -8,7 +8,7 @@ public class UserDaoImpl implements UserDao {
 	public String delete(String arg) {
 		
 		String str = null;
-		str.length(); //用来测试throwadvice
+		//str.length(); //用来测试throwadvice
 		System.out.println("删除成功!"+arg);
 		
 		return "del ok";
